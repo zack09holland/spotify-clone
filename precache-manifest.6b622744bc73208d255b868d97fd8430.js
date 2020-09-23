48135842831116285a159d8a6738bafd56e8a714
Zach Holland <zack09holland@gmail.com>
2020-09-23 13:42:27 -0700

    Updates

---
+++ precache-manifest.6b622744bc73208d255b868d97fd8430.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc4c60850faf5e61d109b5878fa5676",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "a53f35c0b3bbaa88113d",
+    "url": "/spotify-clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b515b96249c1687b6457",
+    "url": "/spotify-clone/static/js/2.c69b6e31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spotify-clone/static/js/2.c69b6e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53f35c0b3bbaa88113d",
+    "url": "/spotify-clone/static/js/main.ec96be23.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotify-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
